b4a136804773f9b93adb77821395e59dfefd2e9d
mukeshlaxmi90 <mukeshlaxmi90@gmail.com>
2025-09-05 12:39:45 +0530

    l

--- my-dashboard/src/App.js
+++ my-dashboard/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter  as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter   as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Dashboard from './Components/Dashboard';
